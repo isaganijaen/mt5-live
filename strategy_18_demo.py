afb83243b660a9990c2d9a6d159db608c92b6e41
isaganijaen <isagani.jaen+github@gmail.com>
2025-09-18 22:44:45 +0800

    strategy_18_demo.py: DISABLING Take Profit Monitoring Thread as this strategy has no definite Target

--- strategy_18_demo.py
+++ strategy_18_demo.py
@@ -499,9 +499,10 @@ def start_strategy():
     position_manager.daemon = True # Allows the thread to exit when the main program exits
     position_manager.start()
 
-    take_profit_monitor = TakeProfitMonitor(config=config_settings, mt5_manager=mt5_manager, position_open_event=position_open_event)
-    take_profit_monitor.daemon = True
-    take_profit_monitor.start()
+    # DISABLING Take Profit Monitoring Thread as this strategy has no definite Target.    
+    # take_profit_monitor = TakeProfitMonitor(config=config_settings, mt5_manager=mt5_manager, position_open_event=position_open_event)
+    # take_profit_monitor.daemon = True
+    # take_profit_monitor.start()
 
     # 4. Instantiate the strategy and run it
     my_strategy = M1AverageZone(config=config_settings, mt5_manager=mt5_manager, position_open_event=position_open_event)
@@ -512,7 +513,7 @@ def start_strategy():
     finally:
         # 5. Shutdown MT5 connection and stop the threads
         position_manager.stop()
-        take_profit_monitor.stop()
+        #take_profit_monitor.stop()
         mt5.shutdown()
         log_success("MetaTrader5 shutdown.")
 
