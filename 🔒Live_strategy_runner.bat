06f4d2f676313655aca287a713c6aa403297d67e
isaganijaen <isagani.jaen+github@gmail.com>
2025-09-26 13:03:10 +0800

    🟢 strategy_14 and 20 will go live

--- 🔒Live_strategy_runner.bat
+++ 🔒Live_strategy_runner.bat
@@ -16,10 +16,10 @@ del /F /Q "*.log" 2>nul
 
 REM Start a new terminal for each script
 REM start "App" cmd /k "python app.py"
-start "Mk"t" cmd /k "python market_data.py"
+start "Mk" cmd /k "python market_data.py"
 start "Database" cmd /k "python database_live.py"  
-start "LIVE - 14" cmd /k "strategy_14_demo.py" 
-start "LIVE - 20" cmd /k "strategy_20_demo.py" 
+start "LIVE - 14" cmd /k "python strategy_14_demo.py" 
+start "LIVE - 20" cmd /k "python strategy_20_demo.py" 
  
 echo All Live Trading Strategies have been launched in separate terminal windows.
 pause
