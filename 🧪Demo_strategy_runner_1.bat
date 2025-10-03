4757563481b8e4f6e9056e5478a32c61d65f5fd2
isaganijaen <isagani.jaen+github@gmail.com>
2025-10-03 15:10:45 +0800

    Archived other strategies

--- 🧪Demo_strategy_runner_1.bat
+++ 🧪Demo_strategy_runner_1.bat
@@ -26,38 +26,42 @@ start "mk" cmd /k "python market_data.py"
 REM -------------------------------------------------------------
 REM ---     BASELINE STAGE (Initial Data Collection)          ---
 REM -------------------------------------------------------------
-start "7" cmd /k "python strategy_07_demo.py"  
+REM start "1" cmd /k "python strategy_01.py"  - Best in 1-4am and 10-5pm only 🟡
+REM start "2" cmd /k "python strategy_02.py" 
+REM start "3" cmd /k "python strategy_03.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "7" cmd /k "python strategy_07_demo.py"  
 REM start "8" ARCHIVED 
 REM start "Str-09" - MOVED to Profitiable Section
 REM start "Str-10" - MOVED to Profitiable Section
-start "11" cmd /k "python strategy_11_demo.py"
-start "12" cmd /k "python strategy_12_demo.py"
+REM start "10" cmd /k "python strategy_10_demo.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "11" cmd /k "python strategy_11_demo.py"
+REM start "12" cmd /k "python strategy_12_demo.py"
 start "13" cmd /k "python strategy_13_demo.py"
-start "14" cmd /k "python strategy_14_demo.py"
-start "15" cmd /k "python strategy_15_demo.py"
-start "16" cmd /k "python strategy_16_demo.py"
+REM start "14" cmd /k "python strategy_14_demo.py"
+REM start "15" cmd /k "python strategy_15_demo.py"
+REM start "16" cmd /k "python strategy_16_demo.py" - Best in 1-4am and 10-5pm only 🟡
 REM start "17" ARCHIVED 
-start "18" cmd /k "python strategy_18_demo.py"
-REM  start "19" - - MOVED to Profitiable Section
-start "20" cmd /k "python strategy_20_demo.py"
-start "21" cmd /k "python strategy_21_demo.py"
-start "22" cmd /k "python strategy_22_demo.py"
-start "23" cmd /k "python strategy_23_demo.py"
-start "24" cmd /k "python strategy_24_demo.py"
-start "25" cmd /k "python strategy_25.py"
+REM start "18" cmd /k "python strategy_18_demo.py"
+REM start "19" cmd /k "python strategy_19_demo.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "20" cmd /k "python strategy_20_demo.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "21" cmd /k "python strategy_21_demo.py"
+REM start "22" cmd /k "python strategy_22_demo.py"
+REM start "23" cmd /k "python strategy_23_demo.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "24" cmd /k "python strategy_24_demo.py" - 24/7 high win rate but negative profit 🟡
+REM start "25" cmd /k "python strategy_25.py"
 start "26" cmd /k "python strategy_26.py"
-start "27" cmd /k "python strategy_27.py"
+REM start "27" cmd /k "python strategy_27.py"
 start "28" cmd /k "python strategy_28.py"
-start "29" cmd /k "python strategy_29.py"
-start "30" cmd /k "python strategy_30.py"
-start "31" cmd /k "python strategy_31.py"
-start "32" cmd /k "python strategy_32.py"
-start "33" cmd /k "python strategy_33.py"
-start "34" cmd /k "python strategy_34.py"
-start "35" cmd /k "python strategy_35.py"
-start "36" cmd /k "python strategy_36.py"
-start "37" cmd /k "python strategy_37.py"
-start "38" cmd /k "python strategy_38.py"
+REM start "29" cmd /k "python strategy_29.py"
+REM start "30" cmd /k "python strategy_30.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "31" cmd /k "python strategy_31.py"
+REM start "32" cmd /k "python strategy_32.py" - Best in 1-4am and 10-5pm only 🟡
+REM start "33" cmd /k "python strategy_33.py"
+REM start "34" cmd /k "python strategy_34.py"
+REM start "35" cmd /k "python strategy_35.py"
+REM start "36" cmd /k "python strategy_36.py"
+REM start "37" cmd /k "python strategy_37.py"
+REM start "38" cmd /k "python strategy_38.py"
 
 
 
@@ -74,14 +78,13 @@ REM -------------------------------------------------------------
 REM ---         CURRENT PROFITABLE STRATEGIES                 ---
 REM -------------------------------------------------------------
 
-start "1" cmd /k "python strategy_01.py" 
-start "2" cmd /k "python strategy_02.py" 
-start "3" cmd /k "python strategy_03.py" 
 
-start "9" cmd /k "python strategy_09_demo.py" 
-start "10" cmd /k "python strategy_10_demo.py" 
 
-start "19" cmd /k "python strategy_19_demo.py"
+
+REM start "9" cmd /k "python strategy_09_demo.py" 
+
+
+
 
 
 
